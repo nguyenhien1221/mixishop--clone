8c67f47bc704ffad4d243876932cd1c269748254
NguyenHien <nguyenhien2942000@gmail.com>
2022-06-28 15:06:53 +0700

    update css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ function Navbar() {
     const [click, setClick] = useState(false)
     
     const handleClick = () => setClick(!click)
+
     const closeMobileMenu = () => setClick(false)
 
     const scrollTop = () =>{
